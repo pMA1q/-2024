3fe664ffb23f50e6684e9d999b5bc25fa7d096c9
nakashima <aiyinzhongdao@gmail.com>
2024-10-11 15:08:35 +0900

    ミッションテーブル更新

--- Pachinko/Assets/Game/Controller/MissionPhase/CSO_MissionPhaseTable.cs
+++ Pachinko/Assets/Game/Controller/MissionPhase/CSO_MissionPhaseTable.cs
@@ -22,11 +22,9 @@ public class MissionPhaseInfomation
     public string name;
     [Header("当落情報")]
     public WIN_LOST win_lost;
-
-    [Header("各ミッションのテクスチャマテリアル")]
-    public Material missionTextureMaterial;
-
-    [Header("演出")]
+    [Header("再抽選有無")]
+    public REPLAY replay;
+    [Header("演出プレハブ")]
     public GameObject performance;
 }
 
@@ -38,4 +36,13 @@ public enum WIN_LOST
     MIDDLE_WIN,//中当たり
     BIG_WIN,//大当たり
     RANDOM//ランダム
+}
+
+//再抽選有無
+public enum REPLAY
+{
+    FALSE = 0,//無
+    TRUE_P1,
+    TRUE_P2
+    
 }
