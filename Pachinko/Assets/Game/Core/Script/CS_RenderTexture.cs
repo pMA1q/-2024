22268ce6ccb6bcbca45bad49c68aab2cd2a911eb
nakashima <aiyinzhongdao@gmail.com>
2024-11-16 16:23:50 +0900

    SAMeshColliderをインポート

--- Pachinko/Assets/Game/Core/Script/CS_RenderTexture.cs
+++ Pachinko/Assets/Game/Core/Script/CS_RenderTexture.cs
@@ -39,7 +39,9 @@ public class CS_RenderTexture : MonoBehaviour
 
         // RenderTextureの設定。カメラのアスペクト比を元に正確な解像度で作成
         int renderTextureWidth = mSubCamera.pixelHeight * Mathf.RoundToInt(aspectRatio);
-        RenderTexture rTexture = new RenderTexture(renderTextureWidth, mSubCamera.pixelHeight, 24);
+        //RenderTexture rTexture = new RenderTexture(renderTextureWidth, mSubCamera.pixelHeight, 24);
+        RenderTexture rTexture = new RenderTexture(1920, 1080, 24);
+        rTexture.depthStencilFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.D16_UNorm;
         mSubCamera.targetTexture = rTexture;
 
         // マテリアルのメインテクスチャをRenderTextureに設定
