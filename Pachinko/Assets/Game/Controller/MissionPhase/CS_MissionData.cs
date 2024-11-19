304ce5c959fc44ecd73431989741f90ea1060c2e
nakashima <aiyinzhongdao@gmail.com>
2024-11-19 18:04:13 +0900

    ミッションデータ更新

--- Pachinko/Assets/Game/Controller/MissionPhase/CS_MissionData.cs
+++ Pachinko/Assets/Game/Controller/MissionPhase/CS_MissionData.cs
@@ -8,6 +8,9 @@ public class CS_MissionData : MonoBehaviour
     private CSO_PlayerStatus mPlayerStatus;
     public CSO_PlayerStatus PlayerStatus { get{ return mPlayerStatus; } }
 
+
+    /// <summary>--------------------------------------------------------------
+    //準備フェーズの選択結果の保存、取得
     public enum MISSION_TYPE
     {
         COLLECT = 0,    //収集
@@ -19,37 +22,93 @@ public class CS_MissionData : MonoBehaviour
     //ミッションの種類を設定、取得
     public MISSION_TYPE MissionNumber
     {
-        set
-        {
-            mMisisonNumber = value;
-        }
-        get
-        {
-            return mMisisonNumber;
-        }
+        set{ mMisisonNumber = value;}
+        get{ return mMisisonNumber;}
     }
 
-    private bool IsChoiceSuccess = false;
-
-    public void ChoiceSuccess()
-    {
-        IsChoiceSuccess = true;
-    }
-    public void ChoiceSuccessReset()
-    {
-        IsChoiceSuccess = false;
-    }
-
+    //準備フェーズで選択されたミッション内容の番号
+    private int[] mMissionContentsNums = new int[3];
     //抽せんされたミッション番号を保存
     public void SaveMissionContents(int _count, int _contentNum)
     {
         mMissionContentsNums[_count] = _contentNum;
     }
-
-    //ミッション内容
-    private int[] mMissionContentsNums = new int[3];
     public int GetMissionContent(int _contentsNum)
     {
         return mMissionContentsNums[_contentsNum];
+    }
+    /// </summary>-----------------------------------------------------------------
+
+    //選択方式演出の選択成功フラグ(項目番号:11,12,18,19)
+    private bool isChoiceSuccess = false;
+    //選択成功
+    public void ChoiceSuccess()
+    {
+        isChoiceSuccess = true;
+    }
+    //選択フラグリセット
+    public void ChoiceSuccessReset()
+    {
+        isChoiceSuccess = false;
+    }
+
+    //プレイヤーバフフラグ(項目番号:20,21,29)
+    public enum PLAYER_BUFF
+    {
+        NONE,       //無
+        WEAK,       //弱
+        STRONG,     //強
+    }
+    private PLAYER_BUFF mPlayerBuff = PLAYER_BUFF.NONE;
+    //プレイヤーバフの種類を設定、取得
+    public PLAYER_BUFF PlayerBuff
+    {
+        set { mPlayerBuff = value; }
+        get { return mPlayerBuff; }
+    }
+
+    //敵のデバフフラグ(項目番号:22)
+    private bool isEnemyDeBuff = false;
+    //敵のデバフを設定、取得
+    public bool IsEnemyDeBuff
+    {
+        set { isEnemyDeBuff = value; }
+        get { return isEnemyDeBuff; }
+    }
+
+    //スキル取得フラグ(項目番号:23)
+    private int mSkill = 0;
+    //スキル数の設定、取得
+    public int Skill
+    {
+        set { mSkill = value; }
+        get { return mSkill; }
+    }
+
+    //敵遭遇高確モードフラグ(項目番号:26)
+    private bool mHighProbabEnemyMode = false;
+    //フラグの設定、取得
+    public bool HighProbabEnemyMode
+    {
+        set { HighProbabEnemyMode = value; }
+        get { return HighProbabEnemyMode; }
+    }
+
+    //ゲーム数延長数(項目番号:27)
+    private int mExtensionGameCount = 0;
+    //追加するゲームカウント数
+    public int ExtensionGameCount
+    {
+        set { mExtensionGameCount = value; }
+        get { return mExtensionGameCount; }
+    }
+
+    //報酬UPフラグ(項目番号:30)
+    private bool mRewardUp = false;
+    //フラグの設定、取得
+    public bool RewardUp
+    {
+        set { mRewardUp = value; }
+        get { return mRewardUp; }
     }
 }
