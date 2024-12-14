ce24e967258e258f117bed6e6363aef2d4a880a5
nakashima <aiyinzhongdao@gmail.com>
2024-12-15 01:03:15 +0900

    突入画面UIアニメーション追加

---
+++ Pachinko/Assets/Game/BossPhase/Prefab/Title/CS_TitleFadeBoss.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CS_TitleFadeBoss : MonoBehaviour
+{
+    
+    public float fadeOutTime = 3.0f;   // フェードアウト時間
+    private CS_Controller bigController; // コントローラーへの参照（フェーズ管理用）
+    public CS_Controller.PACHINKO_PHESE nextPhase; // 遷移先のフェーズを指定
+
+    private void OnEnable()
+    {
+        bigController = GameObject.Find(CS_CommonData.BigControllerName).GetComponent<CS_Controller>();
+        // フェードアウト開始
+        StartCoroutine(FadeOutPanelAndTransition());
+    }
+
+    private IEnumerator FadeOutPanelAndTransition()
+    {
+        yield return new WaitForSeconds(fadeOutTime);
+
+        Debug.Log("3秒たった");
+        // フェーズを指定されたフェーズに変更
+        if (bigController != null)
+        {
+            bigController.ChangePhase(CS_Controller.PACHINKO_PHESE.BOSS);
+            bigController.CreateController();
+            Debug.Log($"{nextPhase} フェーズへ移行しました");
+        }
+        else
+        {
+            Debug.LogWarning("CS_Controller が設定されていません");
+        }
+        // フェードアウトが完了したらオブジェクトを消去
+        GameObject rootObject = transform.root.gameObject;
+        Destroy(rootObject);
+
+
+       
+    }
+}
