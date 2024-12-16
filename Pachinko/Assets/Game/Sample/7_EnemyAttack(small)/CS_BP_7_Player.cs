9e2d7f0ce608ad69dd3c2ba468f3e3bef610f2a8
kazuya <kazuya.fujisawa828@gmail.com>
2024-12-16 11:58:39 +0900

    a

---
+++ Pachinko/Assets/Game/Sample/7_EnemyAttack(small)/CS_BP_7_Player.cs
@@ -0,0 +1,35 @@
+//-----------------------------
+//プレイヤー攻撃(ボスフェーズ項目番号:7)
+//-----------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_BP_7_Player : MonoBehaviour
+{
+    [SerializeField, Header("EnemyのTransform")]
+    private Transform mTarget;
+
+    private bool IsMoving = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsMoving) { return; }
+        float moveSpeed = 15f;
+        transform.position = Vector3.MoveTowards(transform.position, mTarget.position, moveSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            IsMoving = false;
+        }
+    }
+}
