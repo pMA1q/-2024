f7395a697e112c0ce40f477f3e84457e6f936db3
Mai Nozaki <nk110208@gmail.com>
2024-12-19 11:27:31 +0900

    mainブランチを一つ前の工程に戻した際の差分を一部適用

--- Pachinko/Assets/nozaki/RoomChoice/Script/SceneAnimatorParentManager.cs
+++ Pachinko/Assets/nozaki/RoomChoice/Script/SceneAnimatorParentManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomSelectAnimatorManager : MonoBehaviour
+public class SceneAnimatorParentManager : MonoBehaviour
 {
 
     [SerializeField, Header("アニメーター")]
---
+++ Pachinko/Assets/nozaki/SubjugationMission/Script/AnimatorManager_SM04.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager_SM04 : MonoBehaviour
+{
+
+    [SerializeField, Header("アニメーター")]
+    Animator sceneAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 右矢印キーでbRightをtrueに設定
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            sceneAnimator.SetBool("bRight", true);
+        }
+
+        // 左矢印キーでbLeftをtrueに設定
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            sceneAnimator.SetBool("bLeft", true);
+        }
+    }
+}
