e9faed9b875b6bed5b6a6ce6e0d494d80c8420d0
Mai Nozaki <nk110208@gmail.com>
2024-12-20 18:34:23 +0900

    討伐ミッション04.05の階層変更によるAnimationのmissing修正及び消えたカメラアニメーションの作り直し

---
+++ Pachinko/Assets/nozaki/SubjugationMission/Script/RoomSelect.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class RoomSelect : MonoBehaviour
+{
+    public Animator cameraAnimator; // カメラ用Animator
+    public GameObject leftEnemy;   // 左の部屋（敵がる場合）
+    public GameObject rightEnemy;  // 右の部屋（敵がいる場合）
+
+    public GameObject leftEmpty;   // 左の部屋（敵がいない場合）
+    public GameObject rightEmpty;  // 右の部屋（敵がいない場合）
+
+    private void Start()
+    {
+        // 敵を有効化
+        //leftEnemy.SetActive;
+        //centerEnemy.SetActive;
+        //rightEnemy.SetActive;
+
+        // 敵がいない場合はEmptyオブジェクトを有効化
+        leftEmpty.SetActive(!leftEnemy.activeSelf);
+        rightEmpty.SetActive(!rightEnemy.activeSelf);
+    }
+
+    public void SelectLeftRoom()
+    {
+        cameraAnimator.SetTrigger("Left");
+        ShowEnemy(leftEnemy, leftEmpty);
+    }
+
+    public void SelectCenterRoom()
+    {
+        cameraAnimator.SetTrigger("Center");
+    }
+
+    public void SelectRightRoom()
+    {
+        cameraAnimator.SetTrigger("Right");
+        ShowEnemy(rightEnemy, rightEmpty);
+    }
+
+    private void ShowEnemy(GameObject enemy, GameObject empty)
+    {
+        if (enemy.activeSelf)
+        {
+            Debug.Log("敵が出現");
+            // 敵がいる場合の追加演出をここに記述
+        }
+        else
+        {
+            Debug.Log("誰もいない");
+            // 敵がいない場合の追加演出をここに記述
+        }
+    }
+}
